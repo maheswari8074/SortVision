9035b16a419e1064ea9d781d3de884c4a33a73f5
alienx5499 <p2005p5499p@gmail.com>
2025-04-06 16:45:20 +0530

    Update import statement for Analytics in main.jsx to use the correct path for React integration.

--- SortVision/src/main.jsx
+++ SortVision/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { HelmetProvider } from 'react-helmet-async'
 import { SpeedInsights } from '@vercel/speed-insights/react'
-import { Analytics } from '@vercel/analytics'
+import { Analytics } from '@vercel/analytics/react'
 import App from './App.jsx'
 import './index.css'
 
