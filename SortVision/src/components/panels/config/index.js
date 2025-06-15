6821c8aabbefe533801cef915ae2208ba06a1117
Maheswari8074 <maheswarimudadla756@gmail.com>
2025-06-15 12:38:04 +0530

    Added  Algorithm Speed Presets for Different Learning Styles #39

--- SortVision/src/components/panels/config/index.js
+++ SortVision/src/components/panels/config/index.js
@@ -1,6 +1,7 @@
 // Export all config components for easier importing
-export { default as AlgorithmSelector } from './AlgorithmSelector';
-export { default as ComplexityInfo } from './ComplexityInfo';
-export { default as ArraySizeControl } from './ArraySizeControl';
-export { default as SpeedControl } from './SpeedControl';
-export { default as ControlButtons } from './ControlButtons'; 
+export { default as AlgorithmSelector } from "./AlgorithmSelector";
+export { default as ComplexityInfo } from "./ComplexityInfo";
+export { default as ArraySizeControl } from "./ArraySizeControl";
+export { default as SpeedControl } from "./SpeedControl";
+export { default as ControlButtons } from "./ControlButtons";
+export { default as SpeedPresets } from "./SpeedPresets";
