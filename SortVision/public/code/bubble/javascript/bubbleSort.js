525a96c1a42b9287ee5234695093ae0d44ed9d37
Himanshu Kabra <kabrahimanshu123@gmail.com>
2025-06-16 16:55:07 +0530

    added test case for already sorted array

--- SortVision/public/code/bubble/javascript/bubbleSort.js
+++ SortVision/public/code/bubble/javascript/bubbleSort.js
@@ -68,6 +68,7 @@ function testCases() {
     {input: [], expected: []},
     {input: [1], expected: [1]},
     {input: [2, 1], expected: [1, 2]},
+    {input: [1, 2, 3, 4, 5], expected: [1, 2, 3, 4, 5]},
     {input: [2, 2, 2], expected: [2, 2, 2]},
     {input: [10, 3, 1, 2, 4, 11, 7, 6], expected: [1, 2, 3, 4, 6, 7, 10, 11]},
     {input: [1, 2, 1, 2, 1, 2, 1], expected: [1, 1, 1, 1, 2, 2, 2]}
